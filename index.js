2cc25cd8f3387ef78b4627a08885f44b5c5e33d4
Matthisk Heimensen <m@tthisk.nl>
2017-08-20 13:07:03 +0200

    Auto resize main/article columns

--- index.js
+++ index.js
@@ -7,12 +7,24 @@ const defaultOptions = {
 module.exports.express = function stack(options) {
   const opts = Object.assign({}, defaultOptions, options);
 
+  if (process.env.NODE_ENV === 'production') {
+    throw new Error(
+      "Error handling middleware should not be used in 'production'"
+    );
+  }
+
   // eslint-disable-next-line global-require
   return require('./src/middleware/express')(opts);
 };
 
 module.exports.koa = function stack(options) {
   const opts = Object.assign({}, defaultOptions, options);
+
+  if (process.env.NODE_ENV === 'production') {
+    throw new Error(
+      "Error handling middleware should not be used in 'production'"
+    );
+  }
 
   // eslint-disable-next-line global-require
   return require('./src/middleware/koa')(opts);
